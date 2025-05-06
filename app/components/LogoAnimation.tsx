e7cf4a3bfd4a10d517f6b2ce6c09d0ca29b9b09b
alban <albansahiti2002@gmail.com>
2025-05-06 10:58:47 +0200

    sdadsd

--- app/components/LogoAnimation.tsx
+++ app/components/LogoAnimation.tsx
@@ -1,24 +1,22 @@
 import React, {useEffect, useRef} from "react";
-import Logo from "../../public/logo-white.png";
 import {gsap} from "gsap";
-import {motion} from "framer-motion";
 
 export default function LogoAnimation() {
   const triangleLeft = useRef<SVGPolygonElement>(null);
   const triangleRight = useRef<SVGPolygonElement>(null);
   const triangleBottom = useRef<SVGPolygonElement>(null);
-  const logoWrapper = useRef<HTMLDivElement>(null);
+  const titleWrapper = useRef<HTMLDivElement>(null);
   const svgRef = useRef<SVGSVGElement>(null);
 
   useEffect(() => {
-    if (!logoWrapper.current) return;
+    if (!titleWrapper.current) return;
 
-    const img = logoWrapper.current.querySelector("img");
-    if (!img) return;
+    if (!titleWrapper.current) return;
 
     gsap.set(triangleRight.current, {x: 300, opacity: 0});
     gsap.set(triangleBottom.current, {y: 300, opacity: 0});
-    gsap.set(img, {scale: 0.5, opacity: 0});
+    gsap.set(titleWrapper.current, {opacity: 0});
+    // gsap.set(img, {scale: 0.5, opacity: 0});
     gsap.set(svgRef.current, {opacity: 1});
 
     const tl = gsap.timeline({delay: 0.5});
@@ -27,7 +25,7 @@ export default function LogoAnimation() {
       x: 0,
       opacity: 1,
       duration: 0.8,
-      ease: "power3.out",
+      // ease: "power3.out",
     })
       .to(
         triangleBottom.current,
@@ -35,60 +33,62 @@ export default function LogoAnimation() {
           y: 0,
           opacity: 1,
           duration: 0.8,
-          ease: "power3.out",
-        },
-        "<"
+          // ease: "power3.out",
+        }
+        // "<"
       )
-      .to(svgRef.current, {
-        opacity: 0,
-        duration: 0,
-      })
-      .to(img, {
+      .to(titleWrapper.current, {
+        x: 0,
         opacity: 1,
-        scale: 1,
-        duration: 0.6,
-        ease: "power2.out",
+        duration: 1,
       });
+    // .to(svgRef.current, {
+    //   opacity: 0,
+    //   duration: 0,
+    // })
+    // .to(img, {
+    //   opacity: 1,
+    //   scale: 1,
+    //   duration: 0.6,
+    //   ease: "power2.out",
+    // });
   }, []);
 
   return (
-    <div className="relative w-full h-screen flex justify-center items-center">
-      <div className="flex justify-center items-center w-full h-full absolute top-0 left-0 z-10">
-        <svg
-          ref={svgRef}
-          viewBox="0 0 500 500"
-          preserveAspectRatio="xMidYMid meet"
-          className="w-full max-w-[500px] h-full max-h-[500px] flex-shrink-0"
+    <div className=" w-full h-screen flex justify-center items-center">
+      <div className="relative  max-w-[500px] top-0 left-0 z-10">
+        <div className="">
+          <svg
+            ref={svgRef}
+            viewBox="0 0 500 500"
+            preserveAspectRatio="xMidYMid meet"
+            className="w-full max-w-[500px] h-full max-h-[500px] flex-shrink-0"
+          >
+            <polygon
+              ref={triangleLeft}
+              points="200,200 247,250 200,298"
+              fill="white"
+            />
+            <polygon
+              ref={triangleRight}
+              points="300,200 252,250 300,297"
+              fill="white"
+              className="opacity-0 translate-x-[300px]"
+            />
+            <polygon
+              ref={triangleBottom}
+              points="203,300 250,254 297,300"
+              fill="white"
+              className="opacity-0 translate-y-[300px]"
+            />
+          </svg>
+        </div>
+        <h1
+          ref={titleWrapper}
+          className="absolute top-52 left-20 opacity-0 text-5xl tracking-widest "
         >
-          <polygon
-            ref={triangleLeft}
-            points="200,200 250,250 200,300"
-            fill="white"
-          />
-          <polygon
-            ref={triangleRight}
-            points="300,200 250,250 300,300"
-            fill="white"
-            className="opacity-0 translate-x-[300px]"
-          />
-          <polygon
-            ref={triangleBottom}
-            points="200,300 250,250 300,300"
-            fill="white"
-            className="opacity-0 translate-y-[300px]"
-          />
-        </svg>
-      </div>
-
-      <div
-        ref={logoWrapper}
-        className="absolute top-1/2 left-1/2 w-1/2 h-1/2 -translate-x-1/2 -translate-y-1/2 z-10"
-      >
-        <motion.img
-          src={Logo.src}
-          alt="Logo"
-          className="w-full h-full object-contain opacity-0 scale-50"
-        />
+          KRAS
+        </h1>
       </div>
     </div>
   );
