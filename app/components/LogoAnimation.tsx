ab3812c72edb267e946036efbca1b5e05976e10b
alban <albansahiti2002@gmail.com>
2025-05-06 11:07:45 +0200

    dsdad

--- app/components/LogoAnimation.tsx
+++ app/components/LogoAnimation.tsx
@@ -16,7 +16,6 @@ export default function LogoAnimation() {
     gsap.set(triangleRight.current, {x: 300, opacity: 0});
     gsap.set(triangleBottom.current, {y: 300, opacity: 0});
     gsap.set(titleWrapper.current, {opacity: 0});
-    // gsap.set(img, {scale: 0.5, opacity: 0});
     gsap.set(svgRef.current, {opacity: 1});
 
     const tl = gsap.timeline({delay: 0.5});
